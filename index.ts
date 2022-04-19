2ac1d4c163b77792395b7f2a91c134e236816f17
Andrew Stacey <koalasthegreat@gmail.com>
2022-04-19 21:14:08 -0230

    add prisma

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("hello world");
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const result = await prisma.cards.findFirst({
+    where: {
+      name: "Amoeboid Changeling"
+    },
+    select: {
+      colors: true,
+      manaCost: true,
+      types: true
+    }
+  });
+
+  console.log(result);
+}
+
+main()
+  .catch((e) => {
+    throw e;
+  });
