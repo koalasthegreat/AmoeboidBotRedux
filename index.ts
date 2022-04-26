b1c08bc1190a61597880b6b81c482df9c3561fab
Andrew Stacey <koalasthegreat@gmail.com>
2022-04-26 21:24:38 -0230

    slash commands init

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import { Client } from "discord.js";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+
 import "dotenv/config"
 
-import { getRuling } from "./src/cards";
+import * as rawCommands from "./src/commands/index"
 
 const client = new Client({
   intents: []
@@ -9,6 +12,42 @@ const client = new Client({
 
 client.login(process.env.DISCORD_TOKEN);
 
-client.on('ready', (d) => {
-  console.log("Client logged into Discord as " + client.user?.username + "#" + client.user?.discriminator);
+client.on('ready', (c) => {
+  console.log("Client logged into Discord as " + c.user?.username + "#" + c.user?.discriminator);
 });
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	if (interaction.commandName === "echo") {
+		const input = interaction.options.getString('input');
+
+		await interaction.reply(input || "No input supplied");
+	}
+})
+
+
+// Place your client and guild ids here
+const clientId = '716768705744470026';
+const guildId = '704080805080727583';
+
+const commands = [];
+commands.push(rawCommands.echo.toJSON());
+
+
+const rest = new REST({ version: '9' }).setToken(process.env.DISCORD_TOKEN || "");
+
+(async () => {
+	try {
+		console.log('Started refreshing application (/) commands.');
+
+		await rest.put(
+			Routes.applicationGuildCommands(clientId, guildId),
+			{ body: commands },
+		);
+
+		console.log('Successfully reloaded application (/) commands.');
+	} catch (error) {
+		console.error(error);
+	}
+})();
---
+++ src/commands/test.ts
@@ -0,0 +1,9 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+
+export const echo = new SlashCommandBuilder()
+	.setName('echo')
+	.setDescription('Replies with your input!')
+	.addStringOption(option =>
+		option.setName('input')
+			.setDescription('The input to echo back')
+			.setRequired(true))
