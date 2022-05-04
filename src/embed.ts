bd0962d7ae01f786f375089c80683d41b0bf4e7a
Andrew Stacey <koalasthegreat@gmail.com>
2022-05-04 20:01:23 -0230

    init embed

---
+++ src/embed.ts
@@ -0,0 +1,12 @@
+import { MessageEmbed } from "discord.js";
+import { Card } from "@prisma/client"
+
+export const createCardEmbed(card: Card) {
+  const cardEmbed = new MessageEmbed()
+    .
+}
+
+export const createRulingEmbed(ruling: any) {
+  const rulingEmbed = new MessageEmbed()
+    .setTitle()
+}
