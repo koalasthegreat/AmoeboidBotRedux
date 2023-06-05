cbf6d499a8c792ff5bfaf4f351feef6c327631b2
Andrew Stacey <koalasthegreat@gmail.com>
2023-06-04 23:24:14 -0230

    add card fetching to command

--- src/commands/card/card.ts
+++ src/commands/card/card.ts
@@ -1,4 +1,5 @@
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Card, Cards, } from "scryfall-sdk";
 import Client from "src/classes/client";
 import { Command } from "src/interfaces";
 
@@ -7,9 +8,17 @@ SlashCommandBuilder
 export default {
   data: new SlashCommandBuilder()
   .setName("card")
-  .setDescription("Fetches a card"),
+  .setDescription("Fetches a card")
+  .addStringOption(option => 
+    option.setName("name")
+      .setDescription("The card name")
+      .setRequired(true)
+  ),
 
   run: async (client: Client, interaction: CommandInteraction) => {
-    interaction.reply(`Card name given was ${interaction}`)
+    const cardName = <string>interaction.options.get('name')?.value || "";
+    const card: Card = await Cards.byName(cardName)
+
+    interaction.reply(`The card's name is ${card.name}`);
   }
 } as Command
