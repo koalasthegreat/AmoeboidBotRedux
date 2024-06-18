53537a749d27824f22ae437cc59b4e4c5c2ab3fe
Andrew Stacey <koalasthegreat@gmail.com>
2024-06-18 16:19:50 -0230

    change id of cached cards

--- prisma/migrations/20240618184732_/migration.sql
+++ prisma/migrations/20240618184732_/migration.sql
@@ -1,7 +1,6 @@
 -- CreateTable
 CREATE TABLE "CachedCard" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "query" TEXT NOT NULL,
+    "query" TEXT NOT NULL PRIMARY KEY,
     "lastQueried" DATETIME NOT NULL,
     "card" TEXT NOT NULL
 );
