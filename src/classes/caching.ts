e232473cf52c8d50db8e37a84efe0b3c6ecd477a
Andrew Stacey <koalasthegreat@gmail.com>
2024-06-25 14:17:48 -0230

    make intermediary function for caching and searching

--- src/classes/caching.ts
+++ src/classes/caching.ts
@@ -1,13 +1,29 @@
 import { Option, none, some } from "fp-ts/lib/Option";
 import { Card } from "scryfall-sdk";
 import { prisma } from "../bot";
-import { CachedCard } from "src/interfaces";
+import { CachedCard, HTTPError } from "../interfaces";
+import { Either } from "fp-ts/lib/Either";
+import { pipe } from "fp-ts/lib/function";
+import { either, option } from "fp-ts";
+import { ScryfallAPI } from "./scryfall";
 
 
 const yesterday = new Date();
 yesterday.setDate(yesterday.getDate() - 1);
 
 export abstract class ScryfallAPICache {
+  public static async getCachedCardOrFetchByName(query: string): Promise<Either<HTTPError, Card>> {
+    const maybeCachedCard = this.getCachedCardByName(query);
+
+    return pipe(
+      await maybeCachedCard,
+      option.fold(
+        async () => await ScryfallAPI.byName(query),
+        async (card) => either.right(card)
+      )
+    )
+  }
+
   public static async getCachedCardByName(query: string): Promise<Option<Card>> {
     const getCachedCard = await prisma.cachedCard.findFirst({
       where: {
